1255c11974f9da06798ed611c0f814679b1a9b71
Pramanik, Ankit <pramanik.85849@gmail.com>
2020-09-02 13:23:33 +0530

    update

---
+++ clothing/src/homepage.component.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const HomePage = () => (
+    <div className="homepage">
+        <div className='directory-menu'>
+            <div className="content">
+                <h1 className='title'>Mens</h1>
+                <spna className='subtitle'>Shop Now</spna>
+            </div>
+        </div>
+        <div className='directory-menu'>
+            <div className="content">
+                <h1 className='title'>Jackets</h1>
+                <spna className='subtitle'>Shop Now</spna>
+            </div>
+        </div>
+        <div className='directory-menu'>
+            <div className="content">
+                <h1 className='title'>Womens</h1>
+                <spna className='subtitle'>Shop Now</spna>
+            </div>
+        </div>
+        <div className='directory-menu'>
+            <div className="content">
+                <h1 className='title'>Sneakers</h1>
+                <spna className='subtitle'>Shop Now</spna>
+            </div>
+        </div>
+        <div className='directory-menu'>
+            <div className="content">
+                <h1 className='title'>Hats</h1>
+                <spna className='subtitle'>Shop Now</spna>
+            </div>
+        </div>
+    </div>
+);
+
+export default HomePage;
