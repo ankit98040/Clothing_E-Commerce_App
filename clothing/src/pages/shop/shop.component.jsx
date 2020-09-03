9a1d8a5c6da9ece922db2de29adb2acac949111d
Pramanik, Ankit <pramanik.85849@gmail.com>
2020-09-04 01:01:30 +0530

    updates

--- clothing/src/pages/shop/shop.component.jsx
+++ clothing/src/pages/shop/shop.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './shop.styles.scss';
 import SHOP_DATA from './shop.data.js';
+import '../../component/collection-preview/collection-preview.component'
 
 class ShopPage extends React.Component {
     constructor(props){
