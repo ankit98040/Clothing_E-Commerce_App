1973cb818485b77a56fcc09df595b7cd528d6055
Pramanik, Ankit <pramanik.85849@gmail.com>
2020-09-03 08:50:21 +0530

    images ready

--- clothing/src/pages/homepage/homepage.component.jsx
+++ clothing/src/pages/homepage/homepage.component.jsx
@@ -1,36 +1,11 @@
 import React from 'react';
 import './homepage.styles.scss';
+import '../../component/directory/directory.component';
+import Directory from '../../component/directory/directory.component';
 
 const HomePage = () => (
   <div className='homepage'>
-    <h1>Welcome to my Homepage</h1>
-    <div className='directory-menu'>
-      
-      <div className='menu-item'>
-        <div className='content'>
-          <div className='title'>JACKETS</div>
-          <span className='subtitle'>SHOP NOW</span>
-        </div>
-      </div>
-      <div className='menu-item'>
-        <div className='content'>
-          <div className='title'>SHOES</div>
-          <span className='subtitle'>SHOP NOW</span>
-        </div>
-      </div>
-      <div className='menu-item'>
-        <div className='content'>
-          <div className='title'>WOMENS</div>
-          <span className='subtitle'>SHOP NOW</span>
-        </div>
-      </div>
-      <div className='menu-item'>
-        <div className='content'>
-          <div className='title'>MENS</div>
-          <span className='subtitle'>SHOP NOW</span>
-        </div>
-      </div>
-    </div>
+      <Directory />
   </div>
 );
 
