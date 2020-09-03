6f4f260c4833e25fd68b5bf18828fc8a826bf9c3
Pramanik, Ankit <pramanik.85849@gmail.com>
2020-09-03 10:06:35 +0530

    styles updates

--- clothing/src/component/menu-item/menu-item.compoment.jsx
+++ clothing/src/component/menu-item/menu-item.compoment.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import './menu-item.styles.scss';
 
 const MenuItem = ({ title, imageUrl, size }) => (
-    <div style={{
-        backgroundImage: `url(${imageUrl})`
-    }} 
+    <div 
     className={`${size} menu-item`} >
+        <div className='background-image' style={{
+        backgroundImage: `url(${imageUrl})`
+    }} />
         <div className='content'>
           <div className='title'>{title.toUpperCase()}</div>
           <span className='subtitle'>SHOP NOW</span>
