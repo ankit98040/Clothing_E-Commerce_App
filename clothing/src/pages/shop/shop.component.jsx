467a46a4b0d4267a513cf7f71a92e61ee8c4bfa7
Pramanik, Ankit <pramanik.85849@gmail.com>
2020-09-04 00:53:01 +0530

    files added

--- clothing/src/pages/shop/shop.component.jsx
+++ clothing/src/pages/shop/shop.component.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import './shop.styles.scss';
+import SHOP_DATA from './shop.data.js';
 
 class ShopPage extends React.Component {
+    constructor(props){
+        super(props);
 
+        this.state = {
+            collections:  SHOP_DATA
+        }
+    }
+    render(){
+        return <div> SHOP PAGE</div>
+    }
 }
 
 export default ShopPage;
