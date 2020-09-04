6426d9e757afeb79e5ea0f4331bf5c35f1d2d83a
Pramanik, Ankit <pramanik.85849@gmail.com>
2020-09-04 16:53:54 +0530

    signin action added

--- clothing/src/component/sign-in/sign-in.component.jsx
+++ clothing/src/component/sign-in/sign-in.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './signin.styles.scss';
+import './sign-in.styles.scss';
 
 class SignIn extends React.Component{
     constructor(props){
@@ -8,19 +8,31 @@ class SignIn extends React.Component{
         this.state = {
             email: '',
             password: ''
-        }
+        };
     }
 
-    return() {
-        this.render(
+    handleSubmit = event => {
+        event.preventDeault();
+
+        this.setState({ email: '', password: '' });
+    } 
+
+    handleChange = event => {
+        const { value, name } = event.target;
+
+        this.setState({ [name]: value })
+    }
+
+    render() {
+        return(
             <div className='sign-in'>
                 <h2>I Already have an account</h2>
                 <span>Sign in with your email and password</span>
 
-                <form>
-                    <input name='email' value={this.state.email} type='email' required />
+                <form onSubmit={this.handleSubmit}>
+                    <input name='email' value={this.state.email} onChange={this.handleChange} type='email' required />
                     <label>Email</label>
-                    <input name='password' type='password' value={this.state.password} required />
+                    <input name='password' type='password' value={this.state.password} onChange={this.handleChange} required />
                     <label>Password</label>
                     <input type='submit' value='Submit Form' />
                 </form>
--- clothing/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ clothing/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import './sign-in-and-sign-up.styles.scss';
+import SignIn from '../../component/sign-in/sign-in.component';
 
 const SignInAndSignUpPage = () => (
-    <div className='sign-in-and-sign-up'>Sign In</div>
+    <div className='sign-in-and-sign-up'>
+        <SignIn />
+    </div>
 );
 
 export default SignInAndSignUpPage;
