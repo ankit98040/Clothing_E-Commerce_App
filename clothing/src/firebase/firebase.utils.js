cb2b475435336ab71875a5c464cb97ce2a763693
Pramanik, Ankit <pramanik.85849@gmail.com>
2020-09-12 13:01:12 +0530

    Update firebase.utils.js

--- clothing/src/firebase/firebase.utils.js
+++ clothing/src/firebase/firebase.utils.js
@@ -14,13 +14,28 @@ const config = {
 
   export const createUserProfileDocument = async (userAuth, additionalData) => {
     if (!userAuth) return;
-
-    const userRef = firestore.doc('users/o2iu7423u4');
-
+  
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+  
     const snapShot = await userRef.get();
-
-    console.log(firestore.doc(snapShot));
-  }
+  
+    if (!snapShot.exists) {
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+      try {
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...additionalData
+        });
+      } catch (error) {
+        console.log('error creating user', error.message);
+      }
+    }
+  
+    return userRef;
+  };
 
   firebase.initializeApp(config);
 
